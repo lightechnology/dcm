1974c4a059ecedea7d0ba3a802d5d6a738294f5b
lzhe <273257774@qq.com>
2017-07-18 01:24:41 +0800

    不理会其他 直接校验和解包 代写

--- src/main/java/org/bdc/dcm/netty/framer/LcmdbFrameDecoder.java
+++ src/main/java/org/bdc/dcm/netty/framer/LcmdbFrameDecoder.java
@@ -22,36 +22,47 @@ public class LcmdbFrameDecoder extends ByteToMessageDecoder {
             // 标记当前位置，以便reset
             in.markReaderIndex();
             // 判断是否是FE A5开头
+            int sum = 0;
+            int curCrcSum = 0;
             if ((byte) 0xFE == in.readByte() && (byte) 0xA5 == in.readByte()) {
-            	byte type = in.readByte();
-            	int packLen = in.readByte() & 0xff;
-            	byte[] data = new byte[packLen];
-            	in.readBytes(data);
-            	byte crcSum = in.readByte();
-            	//不包涵两个头部信息
-            	int sum = (type&0xff) + packLen;
-            	
-            	for(int i=0;i<data.length;i++){
-            		sum+=(data[i] & 0xff);
-            	}
-            	byte calcSum = (byte)(sum & 0xff);
-            	if(crcSum == calcSum){
-            		in.resetReaderIndex();
-            		//两字节头 +1 字节类型 + 1字节长度 +1 字节校验和
-            		ByteBuf frame = in.readBytes(4+packLen+1);
-            		byte[] frameBytes = new byte[frame.readableBytes()];
-            		frame.markReaderIndex();
-            		frame.readBytes(frameBytes);
-            		//防止单独设置时 返回的数据
-        			//if(packLen == 45 || packLen == 9){//只解轮训状态的包
-	            		frame.resetReaderIndex();
-	            		out.add(frame);
-        			//}
-            	}  
-            }else{
-            	in.resetReaderIndex();
-            	in.readableBytes();
+	            sum = in.readByte()&0xff;
+	            curCrcSum = in.readByte() & 0xff;
+	            while(curCrcSum != sum && 1 < in.readableBytes()){
+	            	sum += curCrcSum;
+	            	curCrcSum = in.readByte() & 0xff;
+	            }
             }
+            
+//            if ((byte) 0xFE == in.readByte() && (byte) 0xA5 == in.readByte()) {
+//            	byte type = in.readByte();
+//            	int packLen = in.readByte() & 0xff;
+//            	byte[] data = new byte[packLen];
+//            	in.readBytes(data);
+//            	byte crcSum = in.readByte();
+//            	//不包涵两个头部信息
+//            	int sum = (type&0xff) + packLen;
+//            	
+//            	for(int i=0;i<data.length;i++){
+//            		sum+=(data[i] & 0xff);
+//            	}
+//            	byte calcSum = (byte)(sum & 0xff);
+//            	if(crcSum == calcSum){
+//            		in.resetReaderIndex();
+//            		//两字节头 +1 字节类型 + 1字节长度 +1 字节校验和
+//            		ByteBuf frame = in.readBytes(4+packLen+1);
+//            		byte[] frameBytes = new byte[frame.readableBytes()];
+//            		frame.markReaderIndex();
+//            		frame.readBytes(frameBytes);
+//            		//防止单独设置时 返回的数据
+//        			//if(packLen == 45 || packLen == 9){//只解轮训状态的包
+//	            		frame.resetReaderIndex();
+//	            		out.add(frame);
+//        			//}
+//            	}  
+//            }else{
+//            	in.resetReaderIndex();
+//            	in.readableBytes();
+//            }
         }
     }
 }
